5d2af108e2ac025cb55b4cc0c12c5727fe30208f
seven332 <seven332@163.com>
2015-12-18 23:40:36 +0800

    Rename BootReceiver to TriggerReceiver

--- app/src/main/java/com/hippo/acbattery/TriggerReceiver.java
+++ app/src/main/java/com/hippo/acbattery/TriggerReceiver.java
@@ -21,7 +21,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class BootReceiver extends BroadcastReceiver {
+public class TriggerReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
